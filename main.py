a17e4fcb5cbf12dcf74d15cb806c164c6540ff50
Shreyas-OwO <shreyas.sable230703@gmail.com>
2020-10-13 03:12:21 +0530

    added sys.argv

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
 import numpy as np
 from cv2 import cv2
-
 import pyperclip
 
 def rgb_to_hex(rgb):
-    
     return '%02x%02x%02x' % rgb  #converting rgb to hex
 
-
-
 def click_event(event, x, y, flags, param):
-    
     if event == cv2.EVENT_LBUTTONDOWN:
         blue= img[y,x,0]  
         green = img[y,x,1]
@@ -20,10 +18,10 @@ def click_event(event, x, y, flags, param):
         pyperclip.copy(strXY)
         cv2.putText(img, strXY, (x, y),font, 1, (0,0,0), 2)
         cv2.imshow('image', img)
-
-
-
-img = cv2.imread("Just Slide.png", 1)   #dir of image
+if len(sys.argv) == 1:
+	img = cv2.imread('Just Slide.png', 1)
+else:
+	img = cv2.imread(sys.argv[1], 1)
 cv2.imshow('image', img)
 cv2.setMouseCallback('image', click_event)
 cv2.waitKey(0)
