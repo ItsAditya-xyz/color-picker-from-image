27de456d242dcf89e1781d0e1e9638d440df106e
Shreyas-OwO <shreyas.sable230703@gmail.com>
2020-10-13 03:41:39 +0530

    main.py

--- main.py
+++ main.py
@@ -5,24 +5,27 @@ import numpy as np
 from cv2 import cv2
 import pyperclip
 
+if len(sys.argv) == 1:
+	picture = 'Just Slide.png'
+else:
+	picture = sys.argv[1]
+img = cv2.imread(picture)
+
 def rgb_to_hex(rgb):
-    return '%02x%02x%02x' % rgb  #converting rgb to hex
+    return '%02x%02x%02x' % rgb	# Converts RGB to Hex. RGB is passed as a tuple.
 
 def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        blue= img[y,x,0]  
-        green = img[y,x,1]
-        red = img[y,x,2]
+        blue= img[y, x, 0]  
+        green = img[y, x, 1]
+        red = img[y, x, 2]
         font = cv2.FONT_HERSHEY_SIMPLEX
-        strXY = '#'+ str(rgb_to_hex((red,blue,green)))  
+        strXY = '#' + str(rgb_to_hex((red,blue,green)))  
         pyperclip.copy(strXY)
-        cv2.putText(img, strXY, (x, y),font, 1, (0,0,0), 2)
-        cv2.imshow('image', img)
-if len(sys.argv) == 1:
-	img = cv2.imread('Just Slide.png', 1)
-else:
-	img = cv2.imread(sys.argv[1], 1)
-cv2.imshow('image', img)
-cv2.setMouseCallback('image', click_event)
+        cv2.putText(img, strXY, (x, y), font, 1, (0, 0, 0), 2)
+        cv2.imshow(picture, img)
+
+cv2.imshow(picture, img)
+cv2.setMouseCallback(picture, click_event)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
